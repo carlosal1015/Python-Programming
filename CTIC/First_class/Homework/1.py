04826a0a70bc4ea517d1875f598fa73b9ab00637
Oromion <caznaranl@uni.pe>
2019-08-02 09:57:01 -0500

    Mejora

--- CTIC/First_class/Homework/1.py
+++ CTIC/First_class/Homework/1.py
@@ -1,17 +1,16 @@
-from datetime import timedelta as tm
-
-hora_salida = [tm(hours=9, minutes=43), tm(hours=11, minutes=19), tm(hours=12, minutes=47), tm(hours=14, minutes=0), tm(hours=15, minutes=45), tm(hours=19, minutes=0), tm(hours=21, minutes=45)]
-hora_llegada = [tm(hours=11, minutes=52), tm(hours=13, minutes=31), tm(hours=15, minutes=19), tm(hours=16, minutes=8), tm(hours=17, minutes=55), tm(hours=21, minutes=20), tm(hours=23, minutes=58)]
+from datetime import datetime as dt
+hora_salida = ['09:43', '11:19', '12:47', '14:0', '15:45', '19:00', '21:45']
+hora_llegada = ['11:52', '13:31', '15:19', '16:8', '17:55', '21:20', '23:58']
 s, l = "Tiempo de partida más caercano es ", "llegada a las s"
 horario = {s: hora_salida, l: hora_llegada}
-
 print("Ingrese la hora en formato de 24 horas:")
-hora_usuario = tm(
-    int(input("Hora: ")),
-    int(input("Minutos: ")),
-	)
 
-for i in range(len(hora_salida)):
-	tmp = hora_usuario -  hora_salida[i]
+hora_usuario= input("Hora: ")
+minuto_usuario = input("Minutos: ")
 
-print(f"{s} {l}.")
+now = dt.strptime(hora_usuario+":"+minuto_usuario, "%H:%M")
+x = min(hora_salida, key=lambda t: abs(now - dt.strptime(t, "%H:%M")))
+#print(x)
+index = hora_salida.index(x)
+print(hora_llegada[index])
+#print(f"{s}{x} {l}.")
