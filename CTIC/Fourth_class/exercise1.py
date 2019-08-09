219b4fe14961059fd77266b13f4798bfd5cd670b
Oromion <caznaranl@uni.pe>
2019-08-09 18:30:18 -0500

    Changes

---
+++ CTIC/Fourth_class/exercise1.py
@@ -0,0 +1,24 @@
+lista_vacia =[]
+lista_vacia
+lista1 = [2, 3,4 , 5, 6, 12, 17, -12]
+lista2 = ['peru', True, 12, 'lunes', 1.345]
+lista2[3]
+lista2[-2]
+'lunes'
+lista2[2] = 24
+lista2
+lista1 + [1,3,5]
+lista1*2
+[2, 3, 4, 5, 6, 12, 17, -12, 2, 3, 4, 5, 6, 12, 17, -12]
+min(lista1)
+max(lista1)
+len(lista1)
+sum(lista1)
+
+lst = [30, 1, 2, 1, 0]
+lst.append(40)
+lst
+lst.insert([1, 43])
+
+
+lst.remove(1)
