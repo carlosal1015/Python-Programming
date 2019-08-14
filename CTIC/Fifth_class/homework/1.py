beaa0d5a3ce68eb7e4248cbeecb894f87cf0e6f5
Oromion <caznaranl@uni.pe>
2019-08-14 14:28:04 -0500

    Advance of homework 6 & 7 & 8

--- CTIC/Fifth_class/homework/1.py
+++ CTIC/Fifth_class/homework/1.py
@@ -1,2 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+def show(fileName):
+	with open(fileName + '.txt', "r+") as f:
+		print(f.read())
+
+
+def delete_string(string, fileName):
+	with open(fileName + '.txt', "r+") as f:
+		f.readline() # The counter increase by one.
+		for line in f:
+			if string in line.split():
+				print(line.split())
+				index = line.split().index(string)
+				print(line.split()[index].replace(string, ""))
+				line = line.replace(string, "")
+				print(line.split())
+				#del line.split()[index]
+
+
+def main():
+	#fileName = input("Ingrese el nombre del archivo: ") + '.txt'
+	fileName = 'v5alumnos2019'
+	#show(fileName)
+	#string = input(f"Ingrese el string a eliminar: ")
+	string = 'H'
+	delete_string(string, fileName)
+	#show(fileName)
+
+
+if __name__ == "__main__":
+	main()
+
+
+"""
+a = [2,3,4,5,6,7,8,9,0]
+xyz = [0,12,4,6,242,7,9]
+print([x for x in xyz if x in a])
+"""
--- CTIC/Fifth_class/homework/2.py
+++ CTIC/Fifth_class/homework/2.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+def add(line , fileName):
+	with open(fileName + '.txt', "r+") as f:
+		pass
+
+
+def main():
+	line1 = "L74	68 	2	UDP	40	H	22,4600343"
+	line2 = "L68	63 	1	ALI	37	M	22,3214285"
+	#fileName = input("Ingrese el nombre del archivo: ") + '.txt'
+	fileName = 'v2alumnos2019'
+	add(line1, fileName)
+	add(line2, fileName)
+
+
+if __name__ == "__main__":
+	main()
