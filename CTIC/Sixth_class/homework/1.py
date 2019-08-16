50aed8598ac10cfa3bc4d2f15a8bb2736e5c2026
Oromion <caznaranl@uni.pe>
2019-08-15 22:16:37 -0500

    Advance

--- CTIC/Sixth_class/homework/1.py
+++ CTIC/Sixth_class/homework/1.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from fraccion import Fraccion as fr
 
-un_medio = fr(numerator = 1, denominator = 2)
+un_medio = fr(numerator = 1, denominator = -2)
 dos_tercios = fr(numerator = 2, denominator = 3)
 
 print(un_medio.sumar(dos_tercios))
@@ -10,3 +10,4 @@ print(dos_tercios.sumar(un_medio))
 
 print(un_medio.multiplicar(dos_tercios))
 print(dos_tercios.multiplicar(un_medio))
+#
--- CTIC/Sixth_class/homework/fraccion.py
+++ CTIC/Sixth_class/homework/fraccion.py
@@ -2,20 +2,27 @@ from math import gcd
 
 class Fraccion(object):
     def __init__(self, numerator, denominator):
-        self.numerator = int(numerator / gcd(abs(numerator), abs(denominator)))
-        self.denominator = int(denominator / gcd(abs(numerator), abs(denominator)))
+        self.numerator = numerator
+        self.denominator = denominator
 
     
     def sumar(self, other):
         summation_numerator = self.numerator*other.denominator + self.denominator*other.numerator
         summation_denominator = self.denominator*other.denominator
-        return f"$\\frac{{{summation_numerator}}}{{{summation_denominator}}}$"
+        common_divisor = gcd(abs(summation_numerator), abs(summation_denominator))
+        summation_numerator //= common_divisor
+        summation_denominator //= common_divisor
 
+        return Fraccion(summation_numerator, summation_denominator)
 
     def multiplicar(self, other):
         product_numerator = self.numerator*other.numerator
         product_denominator = self.denominator*other.denominator
-        return f"$\\frac{{{product_numerator}}}{{{product_denominator}}}$"
+        common_divisor = gcd(abs(product_numerator), abs(product_denominator))
+        product_numerator //= common_divisor
+        product_denominator //= common_divisor
+
+        return Fraccion(product_numerator, product_denominator)
 
 
     def __pos__(self, other):
@@ -27,7 +34,10 @@ class Fraccion(object):
 
     
     def __eq__(self, other):
-        pass
+        firstNumber = self.numerator*other.denominator
+        secondNumber = self.denominator*other.numerator
+
+        return firstNumber == secondNumber
 
     def __lt__(self, other):
         pass
